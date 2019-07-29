7024f6ea02b8f9228820e6e0d470ed2c7e02878d
sirm2z <sirm2zisv@gmail.com>
2019-07-29 11:24:20 +0800

    feature: add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   lintOnSave: 'error',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ppt/'
+    : '/'
   // runtimeCompiler: process.env.NODE_ENV !== 'production'
 }
